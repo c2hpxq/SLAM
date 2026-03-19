89c5903fdc5ed731c08efd0322ce29bd3f6eb8c0
c2hpxq <chc_ruc@163.com>
2026-03-20 01:27:41 +0800

    temp line-fitting

--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,32 @@ author = 'c2hpxq'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.mathjax',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh'
 
+# Allow MathJax to render formulas written as $...$ and $$...$$ in rst text.
+mathjax3_config = {
+    'tex': {
+        'inlineMath': [['\\(', '\\)'], ['$', '$']],
+        'displayMath': [['\\[', '\\]'], ['$$', '$$']],
+    }
+}
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Always load MathJax on HTML pages so formulas written as $...$/$$...$$
+# inside rst plain text can still be rendered by the browser.
+html_js_files = [
+    'mathjax-config.js',
+    'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
+]
--- source/posts/first-post.rst
+++ source/posts/first-post.rst
@@ -5,7 +5,36 @@
 
 直线拟合
 ------------------------
+直线方程可以由$x = td + p$表示，其中d是单位长度的方向向量，p是直线上的一点。
+我们可以通过最小二乘法来拟合直线：点到直线的距离的平方和最小。
 
+$$
+L = \\sum_{i=1}^n \\lvert x_i - p \\rvert^2 - ((x_i - p)^T d)^2
+$$
+求解的问题就是
+$$
+\\min_{d, p} L
+$$
+
+我们可以通过求导来求解d和p。
+
+$$
+\\frac{\\partial L}{\\partial p} = 0
+$$
+
+$$
+\\begin{aligned}
+dL
+&= \\sum_{i=1}^n 2 (x_i - p)^T \\mathrm{d}(x_i - p) - 2 (x_i - p)^T d \\cdot \\mathrm{d}\\!\\left((x_i - p)^T d\\right) \\\\
+&= \\sum_{i=1}^n -2 (x_i - p)^T \\mathrm{d}p + 2 (x_i - p)^T d \\cdot d^T \\mathrm{d}p \\\\
+&= (2\\sum_{i=1}^n (x_i - p)^T (d \\cdot d^T - I)) \\mathrm{d}p 
+\\end{aligned}
+$$
+由于$ dL =(\\frac{\\partial L}{\\partial p})^T \\mathrm{d}p $ ，所以有
+$$ \\frac{\\partial L}{\\partial p} =2(d \\cdot d^T - I)\\sum_{i=1}^n (x_i - p) = 0 $$
+
+故有
+$$ p = \\frac{1}{n}\\sum_{i=1}^n x_i $$
 
 平面拟合
 ------------------------
